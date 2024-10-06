031df75ceea9fac8055f921e387c8297b139ce44
Nishan Bhattarai <30421237@students.atmc.edu.au>
2024-10-06 22:30:56 +0545

    add dynamic about us page

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const AboutUs = () => {
+const AboutUs = ({ data, visiondata }) => {
+  data = data[0];
+  visiondata = visiondata[0];
+
+  console.log(visiondata);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="relative h-[50vh] bg-cover bg-center" style={{ backgroundImage: "url('https://qtpremium.com.au/wp-content/uploads/2022/09/aboutus.jpg')" }}>
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="relative z-10 flex flex-col items-center justify-center h-[50vh] p-8">
+          <h1 className="text-4xl font-bold text-white mb-4">About Us</h1>
+          <nav className="text-white mb-4">
+            <Link className="hover:underline" href="/">
+              Home
+            </Link> &gt; <span>{data.title}</span>
+          </nav>
+        </div>
+      </div>
 
-export default AboutUs
+      <div className="p-8">
+        <h2 className="text-2xl text-gray-600 font-bold mb-4">Introduction</h2>
+        <p className="mb-4 text-gray-600">
+          {data.introduction}
+        </p>
+      </div>
+
+      <div className="p-8 flex">
+        <div className="w-1/6 flex flex-col">
+          <img src={data.founders[0].image} alt="Person" className="mb-4" />
+          <h3 className="text-xl font-bold">{data.founders[0].name}</h3>
+          <p className="text-gray-600 mb-2">{data.founders[0].title}</p>
+          <p className="text-gray-600" dangerouslySetInnerHTML={{ __html: data.founders[0].short_description }}></p>
+        </div>
+        <div className="w-5/6 pl-8">
+          <p className="text-gray-600 mb-4 contents" dangerouslySetInnerHTML={{ __html: data.founders[0].long_description}}></p>
+        </div>
+      </div>
+
+      <div className="p-8 flex">
+        <div className="w-5/6">
+          <p className="text-gray-600 mb-4 contents" dangerouslySetInnerHTML={{ __html: data.founders[1].long_description}}></p>
+        </div>
+        <div className="w-1/6 flex flex-col pl-8">
+          <img src={data.founders[1].image} alt="Person" className="mb-4" />
+          <h3 className="text-xl font-bold">{data.founders[1].name}</h3>
+          <p className="text-gray-600 mb-2">{data.founders[1].title}</p>
+          <p className="text-gray-600" dangerouslySetInnerHTML={{ __html: data.founders[1].short_description }}></p>
+        </div>
+      </div>
+
+      <div className="p-8 max-w-[900px] mb-6 mx-auto">
+        <h2 className="text-2xl text-[#3466ad] text-center text-gray-600 font-bold mb-6">{visiondata.introduction}</h2>
+        <div className="grid mt-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {visiondata.visions.map((item, index) => (
+            <Link className="flex flex-col items-center text-center" key={index} href='#'>
+              <img src={item.icon} alt={item.title} className="w-16 h-16 mb-4" />
+              <h3 className="text-xl font-bold text-gray-600">{item.title}</h3>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/aboutus/contents`);
+  const data = await response.json();
+
+  const vision = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/ourvision/contents`);
+  const visiondata = await vision.json();
+  return {
+    props: {
+      data,
+      visiondata
+    },
+  };
+};
+
+export default AboutUs;
