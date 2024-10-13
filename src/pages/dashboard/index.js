0490a19e4e7c6c6ace15ec68be47a22a0a822e89
Nishan Bhattarai <30421237@students.atmc.edu.au>
2024-10-13 16:58:30 +0545

    add edit profile details

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { FaEdit } from 'react-icons/fa';
+import { setTokens } from '@/store/authSlice';
 import { FaUser, FaPlane, FaFileAlt, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Dashboard = () => {
+  const accessToken = useSelector((state) => state.auth.accessToken);
+  const refreshToken = useSelector((state) => state.auth.refreshToken);
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [passwordError, setPasswordError] = useState('');
+  const [nameError, setNameError] = useState('');
+
   const [activeContent, setActiveContent] = useState("profile");
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 2;
@@ -13,6 +27,71 @@ const Dashboard = () => {
     { destination: "London", date: "October 5, 2023", flightNumber: "BA3456" },
     { destination: "Sydney", date: "November 20, 2023", flightNumber: "QF7890" }
   ];
+
+  const [formData, setFormData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value
+    }));
+  };
+
+  const handleProfileUpdate = async (e) => {
+    e.preventDefault();
+    let err = false;
+    if ((formData.password || formData.confirmPassword) && (formData.password !== formData.confirmPassword)) {
+      setPasswordError('Passwords do not match');
+      err = true;
+    }
+    if (formData.name == '') {
+      err = true;
+      setNameError('Name is required');
+    }
+    if (err) {
+      return;
+    }
+
+    setNameError('');
+    setPasswordError('');
+    setSuccessMessage('');
+    setErrorMessage('');
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/users/change-profile-details`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        dispatch(setTokens({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            user: {
+              name: formData.name,
+              email: user.email,
+              id: user.id
+            }
+          }));
+        setSuccessMessage('Profile updated successfully');
+      } else {
+        setErrorMessage('Profile update failed');
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMessage('Profile update failed');
+    }
+  };
 
   const taxReturnsData = [
     { year: "2022", status: "Submitted", refundAmount: 1500 },
@@ -53,11 +132,101 @@ const Dashboard = () => {
         return (
           <div className="space-y-4">
             <h2 className="text-2xl font-bold">My Profile</h2>
-            <p>Name: John Doe</p>
-            <p>Email: john.doe@example.com</p>
-            <p>Location: New York, USA</p>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <button
+              className="bg-[#33AE64] hover:bg-[#299755] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center space-x-2"
+              onClick={() => {
+                setActiveContent("change-profile-details");
+              }}
+            >
+              <FaEdit className="text-white" />
+              <span>Change Profile Details</span>
+            </button>
           </div>
         );
+        case "change-profile-details":
+          return (
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold">Change Profile Details</h2>
+              <form onSubmit={handleProfileUpdate}>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="Your name"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
+                  {nameError && <p className="text-red-500 mt-2">{nameError}</p>}
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Your email"
+                    value={formData.email}
+                    disabled
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    New Password
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    placeholder="New password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+                    Confirm Password
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="Confirm password"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                  />
+                  {passwordError && <p className="text-red-500 mt-2">{passwordError}</p>}
+                </div>
+                <div className="flex items-center justify-between">
+                  <button
+                    className="bg-[#33AE64] hover:bg-[#299755] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    Update Profile
+                  </button>
+                </div>
+              </form>
+
+              {errorMessage && (
+                <div className="mt-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+                  {errorMessage}
+                </div>
+              )}
+
+              {successMessage && (
+                <div className="mt-4 p-4 bg-green-100 border border-green-400 text-green-700 rounded">
+                  {successMessage}
+                </div>
+              )}
+            </div>
+          );
       case "flights":
         const startIndex = (currentPage - 1) * itemsPerPage;
         const paginatedFlights = flightsData.slice(startIndex, startIndex + itemsPerPage);
@@ -106,7 +275,7 @@ const Dashboard = () => {
           <div className="w-full sm:w-1/3 bg-gray-50 p-6 space-y-4">
             <button
               className={`w-full flex items-center justify-start space-x-3 p-3 rounded-lg transition duration-300 ${
-                activeContent === "profile"
+                activeContent === "profile" || activeContent == "change-profile-details"
                   ? "bg-[#64AF33] text-white"
                   : "bg-white hover:bg-gray-200"
               }`}
