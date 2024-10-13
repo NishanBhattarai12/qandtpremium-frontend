a84f32237ad5a0abc6cb93ae4551690418fecee2
Nishan Bhattarai <30421237@students.atmc.edu.au>
2024-10-13 20:08:40 +0545

    change icons

--- src/components/home/Customer.js
+++ src/components/home/Customer.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { FaUser, FaTrophy, FaSpinner } from 'react-icons/fa';
+import { FaUser, FaTrophy, FaUsers } from 'react-icons/fa';
 
 function Customer() {
   return (
     <div className="bg-gray-100 py-12">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
-          
-          {/* Customer Icon with Text */}
           <div className="text-center">
             <FaUser className="text-green-600 text-6xl mx-auto mb-4" />
             <p className="text-lg font-semibold text-gray-700 max-w-xs">
@@ -15,7 +13,6 @@ function Customer() {
             </p>
           </div>
 
-          {/* #1 Icon with Text */}
           <div className="text-center">
             <FaTrophy className="text-yellow-500 text-6xl mx-auto mb-4" />
             <p className="text-lg font-semibold text-gray-700 max-w-xs">
@@ -23,14 +20,12 @@ function Customer() {
             </p>
           </div>
 
-          {/* Loading Icon with 1000+ Customers */}
           <div className="text-center">
-            <FaSpinner className="text-blue-500 text-6xl mx-auto mb-4 animate-spin" />
+            <FaUsers className="text-blue-500 text-6xl mx-auto mb-4" />
             <p className="text-lg font-semibold text-gray-700 max-w-xs">
               1000+ customers all across Melbourne.
             </p>
           </div>
-          
         </div>
       </div>
     </div>
